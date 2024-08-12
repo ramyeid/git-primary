a5ce01c15a7df2eada92219fb06807694c2f6fdd
Ramy Eid <ramy.eid@almentor.net>
2024-08-12 12:04:06 +0300

    My New Commit

--- app.py
+++ app.py
@@ -1,4 +1,31 @@
 print('Hello World')
+print('Hello World')
+print('Hello World')
+print('Hello World')
+print('Hello World')
+print('Hello World')
+print('Hello World')
+print('Hello World')
+
+
+----------|>main
+          |----|> dev
+               |------> my_new_branch_from_dev
 
 
 
+----------|>main
+          |----|----|----|----|----|> mydev
+          |---|> myNewDev
+
+
+From this
+----------|--|>main
+             |----|----|----|----|----|> mydev
+
+To That
+----------|--|>main
+             |----|----|----|----|----|> mydev
+
+
+git rebase main
